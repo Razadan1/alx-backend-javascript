0c69786a05d317f528d3898136e4054939fa5b55
Razadan1 <danmoleomomayowa@gmail.com>
2024-10-02 16:32:30 +0100

    Task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,10 +2,10 @@ export default function taskBlock(trueOrFalse) {
   let task = false;
   let task2 = true;
 
-  // if (trueOrFalse) {
-  //   task = true;
-  //   task2 = false;
-  // }
+  if (trueOrFalse) {
+    task = false;
+    task2 = true;
+  }
 
   return [task, task2];
 }
